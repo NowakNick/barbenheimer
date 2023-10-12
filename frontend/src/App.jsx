0fed0fd2f781277df3d6730dbda31d18018494ee
Nick Nowak <nick.nowak@hs-furtwangen.de>
2023-10-12 10:24:47 +0200

    inital setup complete

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+// Components
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Setup complete</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
