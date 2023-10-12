0e53ae1dc2b84218667f8e8a71e0f505bd9d71e6
Nick Nowak <nick.nowak@hs-furtwangen.de>
2023-10-12 15:07:39 +0200

    refactored classes to functions

--- frontend/src/views/Edit.jsx
+++ frontend/src/views/Edit.jsx
@@ -1,16 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import Navbar from "../components/navbar";
 
-class Edit extends Component {
-  state = {};
-  render() {
-    return (
-      <div className="edit">
-        <Navbar />
-        <h1>Edit</h1>
-      </div>
-    );
-  }
+export default function Edit() {
+  return (
+    <div className="edit">
+      <Navbar />
+      <h1>Edit {id}</h1>
+    </div>
+  );
 }
-
-export default Edit;
